3bb2ad26f0ba8b852de7bb825f62e7b71561257c
mirfanrafif17 <mirfanrafif17@gmail.com>
2021-11-30 17:32:24 +0700

    nambah readme

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const fs = require('fs')
 
 fs.readFile('./laiba-backend-app.2021-11-29.private-key.pem', 'utf-8', (err, data) => {
     //iat
-    console.log(data)
 
     const date = Math.floor(new Date().getTime() / 1000)
 
@@ -13,7 +12,6 @@ fs.readFile('./laiba-backend-app.2021-11-29.private-key.pem', 'utf-8', (err, dat
         iss: "APP_ID"
     }
 
-    console.log(payload)
     const jwt_token = jwt.sign(payload, data, { algorithm: 'RS256' })
     console.log(jwt_token)
 })
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Github App JWT Token
+
+Ini dibuat soalnya di dokumentasi Github App pake ruby. 
+Dan saya gabisa ruby sama sekali. Jadi saya buat ulang pake nodejs aja.
